8395122c696f6f4e37b77cb7cc26c33b55ad7754
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-09-15 22:02:53 -0300

    primeiro commit

---
+++ exercicio2/script.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 3, 4, 5];
+
+const multiplicar = numeros.map((numero) => numero * 3);
+
+console.log(multiplicar);
+
+const dividir = numeros.map((numero) => numero / 2)
+
+console.log(dividir);
